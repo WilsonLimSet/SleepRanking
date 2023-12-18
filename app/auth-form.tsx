844d9330e06d081ecc305c00094eb75dc66a0fea
Wilson Lim Setiawan <wilsonlimsetiawan@gmail.com>
2023-12-17 22:55:44 -0800

    retry deployment

--- app/auth-form.tsx
+++ app/auth-form.tsx
@@ -1,36 +1,30 @@
-
-
-import { createClient } from '@/utils/supabase/server'
-import Link from 'next/link'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-
-
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function AuthForm() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
-
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
 
   const goProfile = async () => {
-    'use server'
+    "use server";
 
-   
-    return redirect('/account')
-  }
-  
+    return redirect("/account");
+  };
+
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
   const signOut = async () => {
-    'use server'
+    "use server";
 
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
-    await supabase.auth.signOut()
-    return redirect('/login')
-  }
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
 
   return user ? (
     <div className="flex items-center gap-4">
@@ -48,6 +42,55 @@ export default async function AuthForm() {
     >
       Sign Up/Login
     </Link>
-  )
+  );
 }
 
+// import { useState, useEffect } from 'react';
+// import { createClient } from "@/utils/supabase/server";
+// import Link from "next/link";
+// import { cookies } from "next/headers";
+// import { useRouter } from 'next/router';
+
+// export default function AuthForm() {
+//   const [user, setUser] = useState(null);
+//   const router = useRouter();
+//   const cookieStore = cookies();
+//   const supabase = createClient(cookieStore);
+
+//   useEffect(() => {
+//     const fetchUser = async () => {
+//       const { data: { user } } = await supabase.auth.getUser();
+//       setUser(user);
+//     };
+
+//     fetchUser();
+//   }, []);
+
+//   const signOut = async () => {
+//     await supabase.auth.signOut();
+//     router.push("/login");
+//   };
+
+//   const goToProfile = () => {
+//     router.push("/account");
+//   };
+
+//   return user ? (
+//     <div className="flex items-center gap-4">
+//       Hey, {user.email}!
+//       <button onClick={goToProfile} className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+//         Profile
+//       </button>
+//       <button onClick={signOut} className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+//         Sign Out
+//       </button>
+//     </div>
+//   ) : (
+//     <Link
+//       href="/login"
+//       className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+//     >
+//       Sign Up/Login
+//     </Link>
+//   );
+// }
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <p className="text-2xl font-bold ">Sleep Ranking</p>
-          {<AuthForm />}
+          <AuthForm />
         </div>
       </nav>
 
