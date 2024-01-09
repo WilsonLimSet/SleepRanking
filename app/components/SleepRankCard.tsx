c83f8f6b247c5c8813b0c2310d360f8e43a39866
Wilson Lim Setiawan <wilsonlimsetiawan@gmail.com>
2024-01-09 08:22:17 -0800

    picture loads, still need to clean up UI and figure out ranking

--- app/components/SleepRankCard.tsx
+++ app/components/SleepRankCard.tsx
@@ -1,11 +1,42 @@
+'use client'
 import Link from "next/link";
+import { Database } from '../database.types'
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card } from "@/components/ui/card";
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+
 
 export default function SleepRankCard({ avatar ,name, score, tracker, country}: { avatar:string, name: string, score: number, tracker: string, country: string}) {
+
+  const supabase = createClientComponentClient<Database>();
+  const [avatarUrl, setAvatarUrl] = useState('');
+
+  useEffect(() => {
+    async function downloadImage(path: string) {
+      try {
+        const { data, error } = await supabase.storage.from('avatars').download(path);
+        if (error) {
+          throw error;
+        }
+        const url = URL.createObjectURL(data);
+        setAvatarUrl(url);
+      } catch (error) {
+        console.error('Error downloading image: ', error instanceof Error ? error.message : String(error));
+      }
+    }
+
+    if (avatar) {
+      downloadImage(avatar);
+    }
+  }, [avatar, supabase]);
+
   return (
+    
+
     <div className="flex flex-col bg-gray-100 dark:bg-gray-800 ">
       <Card>
         <div className="flex items-center space-x-4 p-4">
@@ -13,7 +44,7 @@ export default function SleepRankCard({ avatar ,name, score, tracker, country}: 
             1
           </Badge>
           <Avatar>
-            <AvatarImage src={avatar} />
+            <AvatarImage src={avatarUrl} />
             <AvatarFallback></AvatarFallback>
           </Avatar>
           <div className="space-y-2">
